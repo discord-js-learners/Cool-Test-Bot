103a9e0ff47d04eb45453f1cb52fb553aa3f0309
Shourjjo Majumder <shourjjomajum@outlook.com>
2022-04-18 19:13:03 +0530

    fixed a lot of things

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Client, Intents } = require("discord.js");
+const config = require('./config.json');
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
 });
@@ -27,4 +28,4 @@ client.on("messageCreate", (message) => {
   
 });
 
-client.login(TOKEN);
+client.login(config.TOKEN);

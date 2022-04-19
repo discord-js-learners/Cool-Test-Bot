92bc86b863084574ed7bdf49db7dc0c5992e292f
Shourjjo Majumder <shourjjomajum@outlook.com>
2022-04-19 12:32:59 +0530

    Added ;invite and fixed some wrong grammer

--- index.js
+++ index.js
@@ -17,13 +17,16 @@ client.on("messageCreate", (message) => {
 
   }
   else if (message.content.startsWith(";help")) {
-    message.channel.send("prefix is ; here are some commands = ping,servers.about");
+    message.channel.send("prefix is ; here are some commands = ping,servers,about,invite");
   }
   else if (message.content.startsWith(";servers")) {
       message.channel.send(`Currently in ${client. guilds. cache. size} servers`)
   }
   else if (message.content.startsWith(";about")) {
       message.channel.send("This is a discord bot made by shourgamer2#6474 for learning js")
+  }
+  else if (message.content.startsWith(";invite")){
+    message.channel.send("Here is the invite link for this bot: https://discord.com/api/oauth2/authorize?client_id=965565479127236618&permissions=8&scope=bot")
   }
   
 });
